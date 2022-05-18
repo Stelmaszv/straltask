a1af3c3240615d641987d454a548553d37cff82b
Stelmaszv <stelmaszv@gmail.com>
2022-05-18 13:06:01 +0200

    Add Model Card

--- payment/models.py
+++ payment/models.py
@@ -29,3 +29,16 @@ class DirectPayment(models.Model):
     amount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
     description = models.TextField()
     iban = models.CharField(max_length=30)
+
+class Card(models.Model):
+    create_at = models.DateTimeField(auto_now=True)
+    currency = models.CharField(
+        max_length=3,
+        choices=GEEKS_CHOICES,
+        default="1",
+    )
+    amount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    description = models.TextField()
+    cartholder_name = models.CharField(max_length=30)
+    cartholder_surname = models.CharField(max_length=30)
+    cart_number = models.CharField(max_length=16)
