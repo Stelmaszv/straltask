94beb26f96296b09abf7995c0af1f4f14740a377
Stelmaszv <stelmaszv@gmail.com>
2022-05-18 16:33:49 +0200

    Add validation for Card

--- payment/models.py
+++ payment/models.py
@@ -54,6 +54,21 @@ class DirectPayment(models.Model):
     description = models.CharField(max_length=20)
     iban = models.CharField(max_length=30)
 
+    def save(self, *args, **kwargs):
+        super(DirectPayment, self).save()
+        PaymentInfo(
+            date=str(self.create_at),
+            type='db',
+            payment_mean='iban',
+            description=self.description,
+            currency=self.currency,
+            amount=self.amount,
+            amount_in_pl=get_corent_var(self.amount)
+        ).save()
+
+    def __str__(self):
+        return self.description
+
 class Card(models.Model):
     create_at = models.DateTimeField(auto_now=True)
     currency = models.CharField(
@@ -66,6 +81,37 @@ class Card(models.Model):
     cartholder_name = models.CharField(max_length=30)
     cartholder_surname = models.CharField(max_length=30)
     cart_number = models.CharField(max_length=16)
+
+    def show_payment_mean(self):
+        def hide(number):
+            return 123
+
+        return self.cartholder_name+' '+self.cartholder_surname+' '+str(hide(self.cart_number))
+
+    def valid_card(self):
+       try:
+           int(self.cart_number)
+       except:
+           raise ValueError("Invalid Card Number !")
+
+       if len(self.cart_number) != 16:
+           raise ValueError("Invalid Card Number !")
+
+    def save(self, *args, **kwargs):
+        self.valid_card()
+        super(Card, self).save()
+        PaymentInfo(
+            date=str(self.create_at),
+            type='card',
+            payment_mean=self.show_payment_mean(),
+            description=self.description,
+            currency=self.currency,
+            amount=self.amount,
+            amount_in_pl=get_corent_var(self.amount)
+        ).save()
+
+    def __str__(self):
+        return self.show_payment_mean()
 
 class PaymentInfo(models.Model):
     date = models.DateTimeField(null=False)
