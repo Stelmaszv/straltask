1db782fb7d7d1d8b767108396cef11113777bd80
Stelmaszv <stelmaszv@gmail.com>
2022-05-18 14:10:39 +0200

    Add order by to view PaymentInfoView

--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,43 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from payment.models import PaymentInfo
 from payment.seralisers import PeymentInfoSeralizer
 
+class APIPrototype(APIView):
+    reverse         = True
+    SerializerClass = None
+    many     = True
+    queryset = ''
+    order_by = ''
+    http_method_names = ['get']
 
-class PaymentInfoView(generics.ListAPIView):
+    def on_query_set(self):
+        pass
 
-    Model = PaymentInfo
-    queryset = PaymentInfo.objects.all()
-    serializer_class = PeymentInfoSeralizer
+    def list(self):
+        self.on_query_set()
+        serializer = self.SerializerClass(self.queryset, many=self.many)
+        if len(self.order_by):
+            list = sorted(
+                serializer.data,
+                key=lambda tup: tup[self.order_by],
+                reverse=self.reverse)
+        else:
+            list= serializer.data
+        return list
+
+    def api_get(self, request, *args, **kwargs):
+        return Response(data=self.list(), status=status.HTTP_200_OK)
+
+    def get(self, request, *args, **kwargs):
+        return self.api_get(request)
+
+
+class PaymentInfoView(APIPrototype):
+
+    SerializerClass = PeymentInfoSeralizer
+    queryset = PaymentInfo.objects
+    reverse = True
+    order_by = 'date'
