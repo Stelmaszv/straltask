28ffb625e9d5e066b108f33344fad003281aa2de
Stelmaszv <stelmaszv@gmail.com>
2022-05-18 13:06:36 +0200

    Add to Card admin view

--- payment/admin.py
+++ payment/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import PayByLink,DirectPayment
+from .models import PayByLink,DirectPayment,Card
 admin.site.register(PayByLink)
 admin.site.register(DirectPayment)
+admin.site.register(Card)
