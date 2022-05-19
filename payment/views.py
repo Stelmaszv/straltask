213fa6337be2b548e196e7916741102393c18d65
Stelmaszv <stelmaszv@gmail.com>
2022-05-19 14:35:03 +0200

    Clean Code

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,6 @@
 import json
 import os
-
-from rest_framework import generics, status
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from payment.models import PaymentInfo,PayByLink,DirectPayment,Card
